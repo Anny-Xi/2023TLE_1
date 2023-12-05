68e5fe565ec9039eb77301c1125e703d59f0dc24
Huseyin Furkan Ozcelik <furkanzelik@gmail.com>
2023-12-05 20:12:59 +0100

    mic,cards

--- laravel/resources/views/arrowView.blade.php
+++ laravel/resources/views/arrowView.blade.php
@@ -10,15 +10,31 @@
 </head>
 <body>
 <h1>VindiQu</h1>
-<div class="arrows">
-    <div class="controller">
-        <img class="arrow-up" src="{{asset(('img/arrow_up.png'))}}" alt="up arrow">
-        <div class="left-right">
-            <img class="arrow-left" src="{{asset(('img/arrow_left.png'))}}" alt="left arrow">
-            <img class="arrow-right" src="{{asset(('img/arrow_right.png'))}}" alt="right arrow">
-        </div>
-        <img class="arrow-down" src="{{asset(('img/arrow_down.png'))}}" alt="down arrow">
+
+    <img class="listeners-group" src="{{asset('img/employee-listening.jpeg')}}" alt="listeners">
+    <img class="microphone" src="{{asset('img/microphone-on.png')}}" alt="microphone">
+
+     <div class="card beginpunt">
+        <p>Beginpunt</p>
+     </div>
+
+    <div class="card middelpunt">
+        <p>Middelpunt</p>
     </div>
-</div>
+
+    <div class="card eindpunt">
+        <p>Eindpunt</p>
+    </div>
+
+    <div class="arrows">
+        <div class="controller">
+            <img class="arrow-up" src="{{asset(('img/arrow_up.png'))}}" alt="up arrow">
+            <div class="left-right">
+                <img class="arrow-left" src="{{asset(('img/arrow_left.png'))}}" alt="left arrow">
+                <img class="arrow-right" src="{{asset(('img/arrow_right.png'))}}" alt="right arrow">
+            </div>
+            <img class="arrow-down" src="{{asset(('img/arrow_down.png'))}}" alt="down arrow">
+        </div>
+    </div>
 </body>
 </html>
