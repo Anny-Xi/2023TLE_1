36f7fea0f1836eff557bb9c3cf3d73cfcece0ce4
AnnyXi <114316992+Anny-Xi@users.noreply.github.com>
2023-12-07 15:55:21 +0100

    Controller system finished
    The (fake) controller system is finished, the image that is being used will not be out of the view.

--- laravel/resources/views/arrowView.blade.php
+++ laravel/resources/views/arrowView.blade.php
@@ -55,6 +55,10 @@
 <script>
     // Get the movable image element
     const movableImage = document.getElementById('movableImage');
+    console.log(movableImage.naturalWidth + ' px ' + movableImage.naturalHeight + ' px');
+
+    let imageValueX = 0 - movableImage.naturalWidth + movableImage.clientWidth;
+    let imageValueY = 0 - movableImage.naturalHeight + movableImage.clientHeight;
 
     // Set the initial position
     let positionX = 0;
@@ -66,27 +70,43 @@
     // Function to update the position of the image
     const updatePosition = () => {
         movableImage.style.objectPosition = `${positionX}px ${positionY}px`;
-        // movableImage.style.top = `${positionY}px`;
     };
 
     // Event listener for arrow key presses
     document.addEventListener('keydown', (event) => {
         switch (event.key) {
             case 'ArrowUp':
-                positionY -= moveAmount;
+                positionY += moveAmount;
+                console.log(positionY);
                 break;
             case 'ArrowDown':
-                positionY += moveAmount;
+                positionY -= moveAmount;
+                console.log(positionY);
                 break;
             case 'ArrowLeft':
-                positionX -= moveAmount;
+                positionX += moveAmount;
+                console.log(positionX);
                 break;
             case 'ArrowRight':
-                positionX += moveAmount;
+                positionX -= moveAmount;
+                console.log(positionX);
                 break;
         }
 
         // Update the position of the image
+
+
+        if (positionX <= imageValueX){
+            positionX = imageValueX;
+        }else if(positionX>0){
+            positionX = 0;
+        }
+        if (positionY <= imageValueY){
+            positionY = imageValueY;
+        }else if (positionY>0){
+            positionY = 0;
+        }
+
         updatePosition();
     });
 </script>
