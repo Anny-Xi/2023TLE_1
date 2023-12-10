7a1289c9acce874416ac73c99ffe4a600f1a47c2
Huseyin Furkan Ozcelik <furkanzelik@gmail.com>
2023-12-10 19:33:32 +0100

    changes css

--- laravel/resources/views/arrowView.blade.php
+++ laravel/resources/views/arrowView.blade.php
@@ -6,35 +6,110 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" type="text/css" href="{{ asset('/css/style.css') }}">
-    <title>Document</title>
+    <title>Live</title>
+
+
 </head>
 <body>
-<h1>VindiQu</h1>
 
+<main>
+    <div class="live">
+        <p id="placeholder">here comes live</p>
+        <img id="movableImage" src="{{asset(('img/museumone.jpg'))}}" alt="Movable Image">
+    </div>
     <img class="listeners-group" src="{{asset('img/employee-listening.jpeg')}}" alt="listeners">
     <img class="microphone" src="{{asset('img/microphone-on.png')}}" alt="microphone">
 
-     <div class="card beginpunt">
-        <p>Beginpunt</p>
-     </div>
+    <div class="cards">
+        <div class="card" id="start">
+            <p>Beginpunt</p>
+        </div>
 
-    <div class="card middelpunt">
-        <p>Middelpunt</p>
-    </div>
+        <div class="card" id="middle">
+            <p>Middelpunt</p>
+        </div>
 
-    <div class="card eindpunt">
-        <p>Eindpunt</p>
+        <div class="card" id="end">
+            <p>Eindpunt</p>
+        </div>
     </div>
 
     <div class="arrows">
-        <div class="controller">
-            <img class="arrow-up" src="{{asset(('img/arrow_up.png'))}}" alt="up arrow">
-            <div class="left-right">
-                <img class="arrow-left" src="{{asset(('img/arrow_left.png'))}}" alt="left arrow">
-                <img class="arrow-right" src="{{asset(('img/arrow_right.png'))}}" alt="right arrow">
-            </div>
-            <img class="arrow-down" src="{{asset(('img/arrow_down.png'))}}" alt="down arrow">
+        <div class="arrow" id="up">
+            <img class="arrow-image" src="{{asset(('img/arrow_up.png'))}}" alt="up arrow">
         </div>
+        <div class="arrow" id="left">
+            <img class="arrow-image" src="{{asset(('img/arrow_left.png'))}}" alt="left arrow">
+        </div>
+        <div class="arrow" id="right">
+            <img class="arrow-image" src="{{asset(('img/arrow_right.png'))}}" alt="right arrow">
+        </div>
+        <div class="arrow" id="down">
+            <img class="arrow-image" src="{{asset(('img/arrow_down.png'))}}" alt="down arrow">
+        </div>
+
     </div>
+
+</main>
+
+<script>
+    // Get the movable image element
+    const movableImage = document.getElementById('movableImage');
+    console.log(movableImage.naturalWidth + ' px ' + movableImage.naturalHeight + ' px');
+
+    let imageValueX = 0 - movableImage.naturalWidth + movableImage.clientWidth;
+    let imageValueY = 0 - movableImage.naturalHeight + movableImage.clientHeight;
+
+    // Set the initial position
+    let positionX = 0;
+    let positionY = 0;
+
+    // Set the amount of pixels to move on each arrow key press
+    const moveAmount = 10;
+
+    // Function to update the position of the image
+    const updatePosition = () => {
+        movableImage.style.objectPosition = `${positionX}px ${positionY}px`;
+    };
+
+    // Event listener for arrow key presses
+    document.addEventListener('keydown', (event) => {
+        switch (event.key) {
+            case 'ArrowUp':
+                positionY += moveAmount;
+                console.log(positionY);
+                break;
+            case 'ArrowDown':
+                positionY -= moveAmount;
+                console.log(positionY);
+                break;
+            case 'ArrowLeft':
+                positionX += moveAmount;
+                console.log(positionX);
+                break;
+            case 'ArrowRight':
+                positionX -= moveAmount;
+                console.log(positionX);
+                break;
+        }
+
+        // Update the position of the image
+
+
+        if (positionX <= imageValueX){
+            positionX = imageValueX;
+        }else if(positionX>0){
+            positionX = 0;
+        }
+        if (positionY <= imageValueY){
+            positionY = imageValueY;
+        }else if (positionY>0){
+            positionY = 0;
+        }
+
+        updatePosition();
+    });
+</script>
+
 </body>
 </html>
