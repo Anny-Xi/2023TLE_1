53cd34bb2f92ecdf985adc60378fca2a2648692e
Jahuan Fortes <marleyjahuan@gmail.com>
2023-12-10 19:21:23 +0100

    added the slider for the waiting screen

--- laravel/resources/views/welcome.blade.php
+++ laravel/resources/views/welcome.blade.php
@@ -30,6 +30,7 @@
                     @endauth
                 </div>
             @endif
+                    {{-- slider start--}}
                 <div class="sound">
                     <svg
                         xmlns="http://www.w3.org/2000/svg"
@@ -49,7 +50,7 @@
                     <div class="sound-icon">
                         <input type="range" />
                     </div>
-
+                    {{-- slider end--}}
                 <div class="mt-16">
                     <div class="grid grid-cols-1 md:grid-cols-2 gap-6 lg:gap-8">
                         <a href="https://laravel.com/docs" class="scale-100 p-6 bg-white dark:bg-gray-800/50 dark:bg-gradient-to-bl from-gray-700/50 via-transparent dark:ring-1 dark:ring-inset dark:ring-white/5 rounded-lg shadow-2xl shadow-gray-500/20 dark:shadow-none flex motion-safe:hover:scale-[1.01] transition-all duration-250 focus:outline focus:outline-2 focus:outline-red-500">
