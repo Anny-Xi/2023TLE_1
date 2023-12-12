0a3431044af1928c282c397c630a3e4d286806fc
AnnyXi <1061555@hr.nl>
2023-12-12 10:51:40 +0100

    script move to file

--- laravel/resources/views/arrowView.blade.php
+++ laravel/resources/views/arrowView.blade.php
@@ -7,6 +7,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" type="text/css" href="{{ asset('/css/style.css') }}">
     <title>Live</title>
+    <script type="text/javascript" src={{asset('/js/app.js')}}""></script>
 
 
 </head>
@@ -17,6 +18,7 @@
         <p id="placeholder">here comes live</p>
         <img id="movableImage" src="{{asset(('img/museumone.jpg'))}}" alt="Movable Image">
     </div>
+
     <img class="listeners-group" src="{{asset('img/employee-listening.jpeg')}}" alt="listeners">
     <img class="microphone" src="{{asset('img/microphone-on.png')}}" alt="microphone">
 
@@ -52,64 +54,6 @@
 
 </main>
 
-<script>
-    // Get the movable image element
-    const movableImage = document.getElementById('movableImage');
-    console.log(movableImage.naturalWidth + ' px ' + movableImage.naturalHeight + ' px');
-
-    let imageValueX = 0 - movableImage.naturalWidth + movableImage.clientWidth;
-    let imageValueY = 0 - movableImage.naturalHeight + movableImage.clientHeight;
-
-    // Set the initial position
-    let positionX = 0;
-    let positionY = 0;
-
-    // Set the amount of pixels to move on each arrow key press
-    const moveAmount = 10;
-
-    // Function to update the position of the image
-    const updatePosition = () => {
-        movableImage.style.objectPosition = `${positionX}px ${positionY}px`;
-    };
-
-    // Event listener for arrow key presses
-    document.addEventListener('keydown', (event) => {
-        switch (event.key) {
-            case 'ArrowUp':
-                positionY += moveAmount;
-                console.log(positionY);
-                break;
-            case 'ArrowDown':
-                positionY -= moveAmount;
-                console.log(positionY);
-                break;
-            case 'ArrowLeft':
-                positionX += moveAmount;
-                console.log(positionX);
-                break;
-            case 'ArrowRight':
-                positionX -= moveAmount;
-                console.log(positionX);
-                break;
-        }
-
-        // Update the position of the image
-
-
-        if (positionX <= imageValueX){
-            positionX = imageValueX;
-        }else if(positionX>0){
-            positionX = 0;
-        }
-        if (positionY <= imageValueY){
-            positionY = imageValueY;
-        }else if (positionY>0){
-            positionY = 0;
-        }
-
-        updatePosition();
-    });
-</script>
 
 </body>
 </html>
