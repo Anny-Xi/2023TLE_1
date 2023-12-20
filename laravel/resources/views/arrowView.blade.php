22cf67d7274d15e87c13d933f04c32d2f9444c5b
AnnyXi <1061555@hr.nl>
2023-12-20 12:26:43 +0100

    add to log-out botton

--- laravel/resources/views/arrowView.blade.php
+++ laravel/resources/views/arrowView.blade.php
@@ -73,6 +73,18 @@
 
     </div>
 
+    <div class="log-out" >
+        <a class="" href="{{ route('logout') }}"
+           onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+            {{ __('Logout') }}
+        </a>
+
+        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+            @csrf
+        </form>
+    </div>
+
 </main>
 
 
