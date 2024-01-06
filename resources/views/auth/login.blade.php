51b1d53cf0f0d6b13de9b887fd3022bd12f6f8d9
AnnyXi <114316992+Anny-Xi@users.noreply.github.com>
2024-01-06 13:59:54 +0100

    home login finish
    home css

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <main>
-        <div id="login-logo">
+        <div id="header-logo">
             <img id="login-logo-image" src="{{asset(('img/vindiqu_logo.png'))}}" alt="logo">
         </div>
         <div class="login_form">
@@ -12,10 +12,10 @@
                     <form method="POST" action="{{ route('login') }}">
                         @csrf
 
-                        <div class="">
+                        <div>
                             <label for="email">{{ __('Email Address') }}</label>
 
-                            <div class="">
+                            <div>
                                 <input id="email" type="email" class="form-control @error('email') is-invalid @enderror"
                                        name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
 
@@ -27,10 +27,10 @@
                             </div>
                         </div>
 
-                        <div class="">
+                        <div>
                             <label for="password">{{ __('Password') }}</label>
 
-                            <div class="">
+                            <div>
                                 <input id="password" type="password"
                                        class="form-control @error('password') is-invalid @enderror" name="password"
                                        required autocomplete="current-password">
@@ -43,34 +43,33 @@
                             </div>
                         </div>
 
-                        <div class="">
-                            <div class="">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember"
-                                           id="remember" {{ old('remember') ? 'checked' : '' }}>
+                        <div>
+                            <div class="form-check">
+                                <input class="form-check-input" type="checkbox" name="remember"
+                                       id="remember" {{ old('remember') ? 'checked' : '' }}>
 
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
-                                </div>
+                                <label class="form-check-label" for="remember">
+                                    {{ __('Remember Me') }}
+                                </label>
                             </div>
+
                         </div>
 
-                        <div class="">
-                            <div class="">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
+                        <div>
+                            <button type="submit" class="btn btn-primary">
+                                {{ __('Login') }}
+                            </button>
 
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
-                            </div>
+                            @if (Route::has('password.request'))
+                                <a class="btn btn-link" href="{{ route('password.request') }}">
+                                    {{ __('Forgot Your Password?') }}
+                                </a>
+                            @endif
                         </div>
+
                     </form>
                 </div>
+                <a href="{{ route('register') }}" class="btn btn-primary">Register here</a>
             </div>
         </div>
     </main>
